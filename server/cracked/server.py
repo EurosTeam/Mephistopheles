47a6e57fa349baf2ccc6bbac1daa02af882c86b1
Mr. Anderson <44845040+call-042PE@users.noreply.github.com>
2020-06-30 17:14:09 +0200

    Add files via upload

--- server/cracked/server.py
+++ server/cracked/server.py
@@ -38,6 +38,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 		config = configparser.ConfigParser()
 		config.read("server\\cracked\\config.ini")
 		forlaxmode = config.get("options","forlaxmode")
+		config = configparser.ConfigParser()
+		config.read("server\\cracked\\config.ini")
+		forlaxmode2 = config.get("options","forlaxmode2")
 		#the basic of a post requests
 		if forlaxmode == "True":
 			#getting the utc time
@@ -54,18 +57,44 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 			#debugging stuff :hi:
 			print(hwid)
 			print(f"{hwid} {secretkey} {corentin} {time}")
-			#																		   	  HWID     SECRETKEY       FORLAXSHIT    UTC TIME
+			#																		   	  HWID     SECRETKEY       FORLAXSTUFF    UTC TIME
 			#exemple of a good cracked.to forlax auth response not encrypted in sha256: 0J94HGB9 3n5blrdzj17ytkc ForlaxWasHere 2020-06-23 17:50 
 			#The secret as always the same len.
 			payload = hashlib.sha256(f"{hwid} {secretkey} {corentin} {time}".encode("utf-8")).hexdigest()
 			#what a payload/response (call it whatever u want) looks like: a486b2af2b7281d304e13a9187c5759bcddc0d4c92a52e3792a4ad982ce9b0d0
-			#important thing if you to custom méphistophélès the space count when you encrypt the response in sha256 so be careful :hi:
+			#important thing if you want to custom méphistophélès the space count when you encrypt the response in sha256 so be careful :hi:
 			print(payload)
 			print(auth+",\"hash\":\""+payload+"\"}")
 			#send the final requests
 			self.wfile.write(bytes(auth+",\"hash\":\""+payload+"\"}","utf-8"))
 
-		if forlaxmode == "False":
+		if forlaxmode2 == "True":
+			#getting the utc time
+			time = datetime.utcnow().strftime("%Y-%m-%d %H:%M")
+			corentin = "ForlaxWasHere"
+			content_length = int(self.headers['Content-Length'])
+			#getting the post params
+			body = self.rfile.read(content_length)
+			self.send_response(200)
+			self.end_headers()
+			#parsing the hwid var of the query
+			hwid = urllib.parse.parse_qs(str(body))['hwid']
+			hwid = listToString(hwid)
+			#debugging stuff :hi:
+			print(hwid[0:-1])
+			print(f"{hwid[0:-1]} {corentin} {time}")
+			#																		   	  HWID    FORLAXSTUFF    UTC TIME
+			#exemple of a good cracked.to forlax auth response not encrypted in sha256: 0J94HGB9 ForlaxWasHere 2020-06-23 17:50 
+			#The secret as always the same len.
+			payload = hashlib.sha256(f"{hwid[0:-1]} {corentin} {time}".encode("utf-8")).hexdigest().title()
+			#what a payload/response (call it whatever u want) looks like: a486b2af2b7281d304e13a9187c5759bcddc0d4c92a52e3792a4ad982ce9b0d0
+			#important thing if you want to custom méphistophélès the space count when you encrypt the response in sha256 so be careful :hi:
+			print(payload)
+			print(auth+",\"hash\":\""+payload+"\"}")
+			#send the final requests
+			self.wfile.write(bytes(auth+",\"hash\":\""+payload+"\"}","utf-8"))
+
+		if forlaxmode and forlaxmode2 == "False":
 			#if is not in forlaxmode he just send normal auth bypass
 			print(forlaxmode)
 			self.send_response(200)
