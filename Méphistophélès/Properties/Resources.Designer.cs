0825fda4c12a394c375821db0184435d9f41c71a
Mr. Anderson <44845040+call-042PE@users.noreply.github.com>
2020-07-23 16:07:49 +0200

    Add files via upload

--- Méphistophélès/Properties/Resources.Designer.cs
+++ Méphistophélès/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace Méphistophélès.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Méphistophélès.Properties {
         ///   Retourne l'instance ResourceManager mise en cache utilisée par cette classe.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Méphistophélès.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace Méphistophélès.Properties {
         ///   les recherches de ressources à l'aide de cette classe de ressource fortement typée.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace Méphistophélès.Properties {
         /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap breaking_or {
+        public static System.Drawing.Bitmap breaking_or {
             get {
                 object obj = ResourceManager.GetObject("breaking-or", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -73,7 +73,7 @@ namespace Méphistophélès.Properties {
         /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap cain {
+        public static System.Drawing.Bitmap cain {
             get {
                 object obj = ResourceManager.GetObject("cain", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -81,9 +81,19 @@ namespace Méphistophélès.Properties {
         }
         
         /// <summary>
+        ///   Recherche une chaîne localisée semblable à [response]
+        ///response=#auth.
+        /// </summary>
+        public static string config {
+            get {
+                return ResourceManager.GetString("config", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap cto {
+        public static System.Drawing.Bitmap cto {
             get {
                 object obj = ResourceManager.GetObject("cto", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -91,12 +101,111 @@ namespace Méphistophélès.Properties {
         }
         
         /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Icon semblable à (Icône).
+        /// </summary>
+        public static System.Drawing.Icon icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap nto {
+        public static System.Drawing.Bitmap nto {
             get {
                 object obj = ResourceManager.GetObject("nto", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à -----BEGIN CERTIFICATE-----
+        ///MIIEKTCCAxGgAwIBAgIUXIwVzez02reK7mKBRRPqR2eWlUowDQYJKoZIhvcNAQEL
+        ///BQAwfDELMAkGA1UEBhMCRlIxCzAJBgNVBAgMAk5ZMREwDwYDVQQHDAhOZXcgWW9y
+        ///azEVMBMGA1UECgwMRXhhbXBsZSwgTExDMRUwEwYDVQQDDAwxOTIuMTY4LjEuNTgx
+        ///HzAdBgkqhkiG9w0BCQEWEHRlc3RAZXhhbXBsZS5jb20wHhcNMjAwNjI0MTY1MTQy
+        ///WhcNMjEwNjI0MTY1MTQyWjB8MQswCQYDVQQGEwJGUjELMAkGA1UECAwCTlkxETAP
+        ///BgNVBAcMCE5ldyBZb3JrMRUwEwYDVQQKDAxFeGFtcGxlLCBMTEMxFTATBgNVBAMM
+        ///DDE5Mi4xNjguMS41ODEfMB0GCSqGSIb3DQEJARYQdGVzdEBleGFtcGxlLmNvbTCC
+        ///ASIwDQYJKoZIhvcNAQEBB [le reste de la chaîne a été tronqué]&quot;;.
+        /// </summary>
+        public static string server_crt {
+            get {
+                return ResourceManager.GetString("server-crt", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à -----BEGIN PRIVATE KEY-----
+        ///MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDRa7mRYnQH7YAg
+        ///NRy6CuunOcQzfIFJ40PzNntahajX+QuukajHjJaOwtRkottsHY2Apf16E/4LrRpJ
+        ///RR5FL/EjJH0aF+KBS55JEa7OJo3Bg7X+mF0Bvr1gcNbl3sdVp2VWJ1S1ZAM1S72k
+        ///+SAgM6Q45olnIContIH643UgiNkNH/hRFE6LpFQSTIP7gn6tmykkztK19S7R/q6B
+        ///YI+QqqnIJSIIac21bS3YVmFDZaNeuzU2fVlwO4Kqu1RhhGjLvnvaJC4iemqHnvMn
+        ///vgOM8FulfZCD3loHdJYBCWIYR0sKapsENT3lTt8Jo33qZxmllbszhyeBov1OUbvR
+        ///OBlUGepNAgMBAAECggEBALTY1VhycAcElze5BgS3N4eiwAdCRpgbFWvrqZyjfgp7
+        ////BWVne4tPzsrjqUVam00H [le reste de la chaîne a été tronqué]&quot;;.
+        /// </summary>
+        public static string server_key {
+            get {
+                return ResourceManager.GetString("server-key", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à from http.server import HTTPServer, BaseHTTPRequestHandler
+        ///import ssl
+        ///
+        ///auth = configparser.ConfigParser()
+        ///auth.read(&quot;config.ini&quot;)
+        ///authresponse = auth.get(&quot;response&quot;,&quot;response&quot;)
+        ///
+        ///class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+        ///
+        ///	def do_GET(self):
+        ///		self.send_response(200)
+        ///		self.end_headers()
+        ///		self.wfile.write(bytes(authresponse,&quot;utf-8&quot;))
+        ///
+        ///	def do_POST(self):
+        ///		self.send_response(200)
+        ///		self.end_headers()
+        ///		self.wfile.write(bytes(authresponse,&quot;utf-8&quot;))
+        ///
+        ///httpd = HTTPServer((&apos;#serve [le reste de la chaîne a été tronqué]&quot;;.
+        /// </summary>
+        public static string Server_Source {
+            get {
+                return ResourceManager.GetString("Server-Source", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à using System;
+        ///using System.Text;
+        ///using System.Diagnostics;
+        ///using System.IO;
+        ///
+        ///namespace Méphistophélès.Resources
+        ///{
+        ///    public class Source
+        ///    {
+        ///        public static string server = &quot;#server&quot;;
+        ///
+        ///        public static void Main()
+        ///        {
+        ///            string server = &quot;C:\\Windows\\System32\\drivers\\etc\\hosts&quot;;
+        ///
+        ///            File.AppendAllText(server, &quot;127.0.0.1 &quot;+server);
+        ///            Process p = new Process();
+        ///            ProcessStartInfo startInfo = new ProcessStartInfo();
+        ///            star [le reste de la chaîne a été tronqué]&quot;;.
+        /// </summary>
+        public static string Source {
+            get {
+                return ResourceManager.GetString("Source", resourceCulture);
             }
         }
     }
